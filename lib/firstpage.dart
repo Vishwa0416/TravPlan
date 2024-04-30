0de4827398bfc3734f8c7c936b6c6e712e1871ac
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-04-30 09:33:23 +0530

    Update in the application

--- lib/firstpage.dart
+++ lib/firstpage.dart
@@ -12,176 +12,26 @@ class First extends StatelessWidget {
         //background
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/signinback.png'),
+            image: AssetImage('assets/firstpageback.png'),
             fit: BoxFit.cover,
           ),
         ),
-        child: Stack(
+        child: const Stack(
           children: [
-            //sign in
-            const Positioned(
-              top: 50,
-              left: 20,
-              child: Text(
-                'SignIn',
-                style: TextStyle(
-                  fontSize: 34,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
             Positioned(
-              //username
-              top: 230,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color:
-                            Color.fromARGB(255, 186, 189, 144).withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.person),
-                      hintText: 'Username',
-                      hintStyle: TextStyle(color: Color.fromARGB(110, 0, 0, 0)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(255, 21, 22, 23),
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              //password
-              top: 320,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 186, 189, 144)
-                            .withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock),
-                      hintText: 'Password',
-                      hintStyle: TextStyle(color: Color.fromARGB(110, 0, 0, 0)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(255, 21, 22, 23),
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const Positioned(
-              //remember me row
-              top: 420,
-              left: 30,
-              child: Row(
-                children: [
-                  Image(image: AssetImage('assets/RememberMe.png')),
-                  SizedBox(width: 10),
-                  Text('Remember Me'),
-                  SizedBox(width: 100),
-                  Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      color: Color.fromARGB(255, 0, 115, 209),
-                      decoration: TextDecoration.underline,
-                      decorationColor: Color.fromARGB(255, 0, 116, 211),
-                    ),
-                  )
-                ],
-              ),
-            ),
-            Positioned(
-                //sign in image
-                top: 480,
-                left: 110,
-                child: GestureDetector(
-                  onTap: () {
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const Home(),
-                      ),
-                    );
-                  },
-                  child: Image(image: AssetImage('assets/signin1.png')),
+                top: 30,
+                left: 330,
+                child: Text(
+                  'Skip',
+                  style: TextStyle(fontSize: 20),
                 )),
             Positioned(
-              //don't have an account
-              top: 560,
-              left: 75,
-              child: Row(
-                children: [
-                  const Text("Don't have an account?"),
-                  const SizedBox(width: 25),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignUp(),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      //sign up
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 0, 115, 209),
-                        decoration: TextDecoration.underline,
-                        decorationColor: Color.fromARGB(255, 0, 116, 211),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const Positioned(top: 580, left: 185, child: Text('or')),
-            const Positioned(
-              //logos
-              top: 610,
-              left: 50,
-              child: Column(
-                children: [
-                  Image(image: AssetImage('assets/Loginfb.png')),
-                  SizedBox(height: 10),
-                  Image(image: AssetImage('assets/Logingoogle.png')),
-                ],
-              ),
-            ),
+                top: 450,
+                left: 100,
+                child: Text(
+                  'Life is short and \nthe world is ',
+                  style: TextStyle(fontSize: 30),
+                ))
           ],
         ),
       ),
