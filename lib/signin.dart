6f62f57d9d8bce3c5c13602c0bd6b129b805d70f
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-05-20 13:29:09 +0530

    Sign Up

--- lib/signin.dart
+++ lib/signin.dart
@@ -1,110 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:test01/signuppage.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Stack(
-      children: [
-        Positioned(
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
             top: 40,
             left: 20,
-            child: Image(image: AssetImage('assets/arrow.png'))),
-        Positioned(
-            top: 150,
-            left: 125,
-            child: Text(
-              'Sign in now',
-              style: TextStyle(fontSize: 25),
-            )),
-        Positioned(
-            top: 190,
-            left: 85,
-            child: Text(
-              'Please sign in to continue our app',
-              style: TextStyle(color: Color.fromARGB(255, 116, 116, 115)),
-            )),
-        //username
-        Align(
-          alignment: Alignment(0, -0.25),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person),
-                hintText: 'Username',
-                hintStyle: TextStyle(color: Color.fromARGB(112, 0, 0, 0)),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  borderSide: BorderSide(
-                    color: Colors.blue, // Border color
-                    width: 2.0, // Border width
+            child: Image.asset('assets/arrow.png'),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 110),
+                  const Text(
+                    'Sign in now',
+                    style: TextStyle(fontSize: 25),
                   ),
-                ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Please sign in to continue our app',
+                    style: TextStyle(color: Color.fromARGB(255, 116, 116, 115)),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      hintText: 'Username',
+                      hintStyle: TextStyle(color: Color.fromARGB(112, 0, 0, 0)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          color: Colors.blue, // Border color
+                          width: 2.0, // Border width
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.lock),
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: Color.fromARGB(112, 0, 0, 0)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 1, 134, 244),
+                          width: 2.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Forgot Password',
+                      style: TextStyle(color: Color.fromARGB(255, 2, 133, 215)),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Image.asset('assets/signin.png'),
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Don’t have an account?'),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignUp(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'Signup',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 2, 133, 215),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 30),
+                  const Text('or connect'),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/fb.png'),
+                      const SizedBox(width: 30),
+                      Image.asset('assets/insta.png'),
+                      const SizedBox(width: 30),
+                      Image.asset('assets/twitter.png'),
+                    ],
+                  ),
+                ],
               ),
             ),
           ),
-        ),
-        //password
-        Align(
-          alignment: Alignment(0, 0.0),
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 20.0),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.lock),
-                hintText: 'Password',
-                hintStyle: TextStyle(color: Color.fromARGB(112, 0, 0, 0)),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  borderSide: BorderSide(
-                    color: Color.fromARGB(255, 1, 134, 244),
-                    width: 2.0,
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ),
-        Positioned(
-            top: 430,
-            left: 250,
-            child: Text(
-              'Forgot Password',
-              style: TextStyle(color: Color.fromARGB(255, 2, 133, 215)),
-            )),
-        Positioned(
-            top: 480,
-            right: 30,
-            child: Image(image: AssetImage('assets/signin.png'))),
-        Positioned(top: 550, left: 90, child: Text('Don’t have an account?')),
-        Positioned(
-            top: 550,
-            left: 250,
-            child: Text('Signup',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 2, 133, 215),
-                ))),
-        Positioned(top: 590, left: 160, child: Text('or connect')),
-        Positioned(
-            top: 630,
-            left: 100,
-            child: Row(
-              children: [
-                Image(image: AssetImage('assets/fb.png')),
-                SizedBox(
-                  width: 30,
-                ),
-                Image(image: AssetImage('assets/insta.png')),
-                SizedBox(
-                  width: 30,
-                ),
-                Image(image: AssetImage('assets/twitter.png'))
-              ],
-            ))
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test01/homepage.dart';
+import 'package:test01/signin.dart';
 
 class SignUp extends StatelessWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -7,282 +7,128 @@ class SignUp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/signinback.png'),
-            fit: BoxFit.cover,
+      body: Stack(
+        children: [
+          Positioned(
+            top: 40,
+            left: 20,
+            child: Image.asset('assets/arrow.png'),
           ),
-        ),
-        child: Stack(
-          children: [
-            //signup text
-            const Positioned(
-              top: 50,
-              left: 20,
-              child: Text(
-                'SignUp',
-                style: TextStyle(
-                  fontSize: 34,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            Positioned(
-              //username
-              top: 110,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 186, 189, 144)
-                            .withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const SizedBox(height: 110),
+                  const Text(
+                    'Sign up now',
+                    style: TextStyle(fontSize: 25),
                   ),
-                  child: const TextField(
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Please fill the details and create account',
+                    style: TextStyle(color: Color.fromARGB(255, 116, 116, 115)),
+                  ),
+                  const SizedBox(height: 20),
+                  const TextField(
                     decoration: InputDecoration(
                       prefixIcon: Icon(Icons.person),
                       hintText: 'Username',
-                      hintStyle: TextStyle(color: Color.fromARGB(110, 0, 0, 0)),
+                      hintStyle: TextStyle(color: Color.fromARGB(112, 0, 0, 0)),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(10)),
                         borderSide: BorderSide(
-                          color: Color.fromARGB(255, 21, 22, 23),
-                          width: 2.0,
+                          color: Colors.blue, // Border color
+                          width: 2.0, // Border width
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            Positioned(
-              //email address
-              top: 190,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 186, 189, 144)
-                            .withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const TextField(
+                  const SizedBox(height: 20),
+                  const TextField(
                     decoration: InputDecoration(
                       prefixIcon: Icon(Icons.email),
-                      hintText: 'Email Address',
-                      hintStyle: TextStyle(color: Color.fromARGB(110, 0, 0, 0)),
+                      hintText: 'E-mail',
+                      hintStyle: TextStyle(color: Color.fromARGB(112, 0, 0, 0)),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(10)),
                         borderSide: BorderSide(
-                          color: Color.fromARGB(255, 21, 22, 23),
-                          width: 2.0,
+                          color: Colors.blue, // Border color
+                          width: 2.0, // Border width
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            Positioned(
-              //phone number
-              top: 270,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 186, 189, 144)
-                            .withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.phone),
-                      hintText: 'Phone Number',
-                      hintStyle: TextStyle(color: Color.fromARGB(110, 0, 0, 0)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(255, 21, 22, 23),
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Positioned(
-              //password
-              top: 350,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 186, 189, 144)
-                            .withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
-                      ),
-                    ],
-                  ),
-                  child: const TextField(
+                  const SizedBox(height: 20),
+                  const TextField(
+                    obscureText: true,
                     decoration: InputDecoration(
                       prefixIcon: Icon(Icons.lock),
                       hintText: 'Password',
-                      hintStyle: TextStyle(color: Color.fromARGB(110, 0, 0, 0)),
+                      hintStyle: TextStyle(color: Color.fromARGB(112, 0, 0, 0)),
                       border: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(10)),
                         borderSide: BorderSide(
-                          color: Color.fromARGB(255, 21, 22, 23),
+                          color: Color.fromARGB(255, 1, 134, 244),
                           width: 2.0,
                         ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            Positioned(
-              //confirm password
-              top: 430,
-              left: 0,
-              right: 0,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color.fromARGB(255, 186, 189, 144)
-                            .withOpacity(1),
-                        spreadRadius: 5,
-                        blurRadius: 7,
-                        offset: const Offset(0, 3),
+                  const SizedBox(height: 20),
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Password must contain atleast 8 characters.',
+                      style:
+                          TextStyle(color: Color.fromARGB(255, 116, 116, 115)),
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Image.asset('assets/signupnew.png'),
+                  const SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text('Already have an account?'),
+                      const SizedBox(width: 5),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignIn(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          'SignIn',
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 2, 133, 215),
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                  child: const TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.lock),
-                      hintText: 'Confirm Password',
-                      hintStyle: TextStyle(color: Color.fromARGB(110, 0, 0, 0)),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(
-                          color: Color.fromARGB(255, 21, 22, 23),
-                          width: 2.0,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            const Positioned(
-              //remember me
-              top: 500,
-              left: 30,
-              child: Row(
-                children: [
-                  Image(image: AssetImage('assets/RememberMe.png')),
-                  SizedBox(width: 10),
-                  Text('Remember Me'),
-                ],
-              ),
-            ),
-            Positioned(
-              //sign in image
-              top: 540,
-              left: 110,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const Home(),
-                    ),
-                  );
-                },
-                child: const Image(image: AssetImage('assets/signup1.png')),
-              ),
-            ),
-            Positioned(
-              //text
-              top: 590,
-              left: 75,
-              child: Row(
-                children: [
-                  const Text("Already have an account?"),
-                  const SizedBox(width: 25),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => const SignUp(),
-                        ),
-                      );
-                    },
-                    child: const Text(
-                      //sign in text
-                      'Sign In',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 0, 115, 209),
-                        decoration: TextDecoration.underline,
-                        decorationColor: Color.fromARGB(255, 0, 116, 211),
-                      ),
-                    ),
+                  const SizedBox(height: 30),
+                  const Text('or connect'),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/fb.png'),
+                      const SizedBox(width: 30),
+                      Image.asset('assets/insta.png'),
+                      const SizedBox(width: 30),
+                      Image.asset('assets/twitter.png'),
+                    ],
                   ),
                 ],
               ),
             ),
-            const Positioned(top: 620, left: 185, child: Text('or')),
-            const Positioned(
-                //google and fblogos
-                top: 660,
-                left: 150,
-                child: Row(
-                  children: [
-                    Image(image: AssetImage('assets/googlelogo.png')),
-                    SizedBox(
-                      width: 40,
-                    ),
-                    Image(image: AssetImage('assets/fblogo.png')),
-                  ],
-                ))
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
