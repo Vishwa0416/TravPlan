2f619091e553235d7c0a3676be9efc0b7979e279
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-05-23 21:28:18 +0530

    Pop-Up Message

--- lib/forgotpassword.dart
+++ lib/forgotpassword.dart
@@ -10,36 +10,37 @@ class Forgot extends StatelessWidget {
       body: Stack(
         children: [
           Positioned(
-              top: 40,
-              left: 20,
-              child: GestureDetector(
-                onTap: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const SignIn(),
-                    ),
-                  );
-                },
-                child: Image.asset('assets/arrow.png'),
-              )),
-          const Center(
+            top: 40,
+            left: 20,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const SignIn(),
+                  ),
+                );
+              },
+              child: Image.asset('assets/arrow.png'),
+            ),
+          ),
+          Center(
             child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30.0),
+              padding: const EdgeInsets.symmetric(horizontal: 30.0),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  Text(
+                  const Text(
                     'Forgot Password',
                     style: TextStyle(fontSize: 25),
                   ),
-                  SizedBox(height: 10),
-                  Text(
+                  const SizedBox(height: 10),
+                  const Text(
                     'Enter your email account to reset  your password',
                     style: TextStyle(color: Color.fromARGB(255, 116, 116, 115)),
                   ),
-                  SizedBox(height: 20),
-                  TextField(
+                  const SizedBox(height: 20),
+                  const TextField(
                     decoration: InputDecoration(
                       prefixIcon: Icon(Icons.email),
                       hintText: 'Email',
@@ -53,16 +54,49 @@ class Forgot extends StatelessWidget {
                       ),
                     ),
                   ),
-                  SizedBox(
+                  const SizedBox(
                     height: 25,
                   ),
-                  Image(image: AssetImage('assets/reset.png'))
+                  GestureDetector(
+                    onTap: () {
+                      _showResetDialog(context);
+                    },
+                    child: const Image(image: AssetImage('assets/reset.png')),
+                  ),
                 ],
               ),
             ),
           ),
         ],
       ),
+    );
+  }
+
+  void _showResetDialog(BuildContext context) {
+    showDialog<void>(
+      context: context,
+      barrierDismissible: true,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Column(
+            children: [
+              Image.asset('assets/emailimg.png'),
+              const SizedBox(height: 10),
+              const Text('Check your email'),
+            ],
+          ),
+          content: const Text(
+              'We have sent password recovery instructions to your email.'),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
