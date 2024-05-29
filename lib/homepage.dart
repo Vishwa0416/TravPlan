9e765bb9f354a2b7de82aa3ce4413f80ec5bca8f
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-05-29 21:32:36 +0530

    Home

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Stack(children: [
-        Positioned(
-            //hello vishwa text
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            // Positioned widget to place the container
             top: 50,
             left: 10,
-            child: Text(
-              'Hello Vishwa',
-              style: TextStyle(fontSize: 30),
-            ))
-      ]),
+            child: Container(
+                width: 150,
+                height: 50,
+                color: Color.fromARGB(255, 183, 183, 183)),
+          ),
+        ],
+      ),
     );
   }
 }
