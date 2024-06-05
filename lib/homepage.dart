e7895d19dc2c9ed7b14e563e60c7f2720b7394e6
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-06-05 18:50:41 +0530

    Home Page Person

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -26,12 +26,21 @@ class Home extends StatelessWidget {
             top: 50,
             left: 10,
             child: Container(
-              width: 150,
-              height: 50,
+              width: 120,
+              height: 50, // Adjusted height to fit the text as well
               decoration: BoxDecoration(
-                  color: Color.fromARGB(255, 183, 183, 183),
-                  borderRadius: BorderRadius.circular(10),
-                  image: AssetImage('assets/person.png')),
+                color: Color.fromARGB(255, 183, 183, 183),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image(image: AssetImage('assets/person.png')),
+                  ),
+                  Text('Vishwa'),
+                ],
+              ),
             ),
           ),
         ],
