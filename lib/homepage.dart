bde9885daf47d025e49850514a0ba4a48785d542
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-06-05 19:02:50 +0530

    Bell

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -25,22 +25,28 @@ class Home extends StatelessWidget {
           Positioned(
             top: 50,
             left: 10,
-            child: Container(
-              width: 120,
-              height: 50, // Adjusted height to fit the text as well
-              decoration: BoxDecoration(
-                color: Color.fromARGB(255, 183, 183, 183),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Image(image: AssetImage('assets/person.png')),
+            child: Row(
+              children: [
+                Container(
+                  width: 120,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 183, 183, 183),
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                  Text('Vishwa'),
-                ],
-              ),
+                  child: const Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Image(image: AssetImage('assets/person.png')),
+                      ),
+                      Text('Vishwa'),
+                    ],
+                  ),
+                ),
+                const SizedBox(width: 200),
+                const Image(image: AssetImage('assets/bell.png')),
+              ],
             ),
           ),
         ],
