3b9b8c120a5785ead82106d74b0960a291336404
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-06-07 17:05:16 +0530

    View Page

--- lib/view.dart
+++ lib/view.dart
@@ -32,6 +32,24 @@ class Views extends StatelessWidget {
               child: Image.asset('assets/arrow.png'),
             ),
           ),
+          const Positioned(
+            top: 40,
+            left: 150,
+            child: Text(
+              'View',
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+          Positioned(
+              top: 95,
+              left: 150,
+              child: Container(
+                  width: 150,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 51, 51, 52),
+                    borderRadius: BorderRadius.circular(15),
+                  )))
         ],
       ),
     );
