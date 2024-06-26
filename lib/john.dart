33ed02e3539043b91c452abb4d07765c981968ee
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-06-26 14:28:01 +0530

    Mic

--- lib/john.dart
+++ lib/john.dart
@@ -63,6 +63,7 @@ class John extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Image(image: AssetImage('assets/mic.png'))
                 ],
               ),
             ),
