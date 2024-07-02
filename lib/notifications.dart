9ba1137fcc499c88f78457e27010de523687300a
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-07-02 13:00:21 +0530

    Note

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:test01/homepage.dart';
+import 'package:test01/john.dart';
 
 class Notifications extends StatelessWidget {
   const Notifications({Key? key}) : super(key: key);
@@ -6,11 +8,97 @@ class Notifications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Notifications'),
-      ),
-      body: Center(
-        child: Text('This is the Notifications Page'),
+      body: Stack(
+        children: [
+          Positioned(
+            top: 40,
+            left: 20,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const Home(),
+                  ),
+                );
+              },
+              child: Row(
+                children: [
+                  Image.asset('assets/arrow.png'),
+                  const SizedBox(
+                    width: 85,
+                  ),
+                  const Text(
+                    'Notifications',
+                    style: TextStyle(fontSize: 25),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Positioned(
+            top: 105,
+            left: 25,
+            child: Row(
+              children: [
+                Text('Notifications'),
+                SizedBox(
+                  width: 250,
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 140,
+            left: 20,
+            right: 20,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const John(),
+                  ),
+                );
+              },
+              child: Container(
+                color: Colors.grey[200],
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/person.png',
+                      width: 50,
+                      height: 50,
+                    ),
+                    const SizedBox(
+                      width: 20,
+                    ),
+                    const Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text(
+                                'John Perera',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                              Spacer(),
+                              Text('9:56'),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          Text('Hi, John! How are you doing?'),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
