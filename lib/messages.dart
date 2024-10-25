5554ddace31ff4df012d3fcee8632df240bae5c5
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-10-26 00:18:59 +0530

    Messages back

--- lib/messages.dart
+++ lib/messages.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:TravPlan/homepage.dart';
 import 'package:TravPlan/john.dart';
 import 'package:TravPlan/bottom_nav_bar.dart';
+import 'background_container.dart';
 
 class Messages extends StatefulWidget {
   const Messages({Key? key}) : super(key: key);
@@ -22,112 +23,114 @@ class _MessagesState extends State<Messages> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned(
-            top: 40,
-            left: 20,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const Home(),
-                  ),
-                );
-              },
-              child: Row(
-                children: [
-                  Image.asset('assets/arrow.png'),
-                  const SizedBox(
-                    width: 85,
-                  ),
-                  const Text(
-                    'Messages',
-                    style: TextStyle(fontSize: 25),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const Positioned(
-            top: 105,
-            left: 25,
-            child: Row(
-              children: [
-                Text('Messages'),
-                SizedBox(
-                  width: 250,
-                ),
-                Image(image: AssetImage('assets/message.png'))
-              ],
-            ),
-          ),
-          const Positioned(
-            top: 160,
-            left: 20,
-            right: 20,
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: 'Search for chats and messages',
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 240,
-            left: 20,
-            right: 20,
-            child: GestureDetector(
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const John(),
-                  ),
-                );
-              },
-              child: Container(
-                color: Colors.grey[200],
-                padding: const EdgeInsets.all(8.0),
+      body: BackgroundContainer(
+        child: Stack(
+          children: [
+            Positioned(
+              top: 40,
+              left: 20,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const Home(),
+                    ),
+                  );
+                },
                 child: Row(
                   children: [
-                    Image.asset(
-                      'assets/person.png',
-                      width: 50,
-                      height: 50,
-                    ),
+                    Image.asset('assets/arrow.png'),
                     const SizedBox(
-                      width: 20,
+                      width: 85,
                     ),
-                    const Expanded(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              Text(
-                                'John Perera',
-                                style: TextStyle(fontSize: 18),
-                              ),
-                              Spacer(),
-                              Text('9:56'),
-                            ],
-                          ),
-                          SizedBox(height: 5),
-                          Text('Hi, John! How are you doing?'),
-                        ],
-                      ),
+                    const Text(
+                      'Messages',
+                      style: TextStyle(fontSize: 25),
                     ),
                   ],
                 ),
               ),
             ),
-          ),
-        ],
+            const Positioned(
+              top: 105,
+              left: 25,
+              child: Row(
+                children: [
+                  Text('Messages'),
+                  SizedBox(
+                    width: 250,
+                  ),
+                  Image(image: AssetImage('assets/message.png'))
+                ],
+              ),
+            ),
+            const Positioned(
+              top: 160,
+              left: 20,
+              right: 20,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Search for chats and messages',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 240,
+              left: 20,
+              right: 20,
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const John(),
+                    ),
+                  );
+                },
+                child: Container(
+                  color: Colors.grey[200],
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        'assets/person.png',
+                        width: 50,
+                        height: 50,
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      const Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                Text(
+                                  'John Perera',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                Spacer(),
+                                Text('9:56'),
+                              ],
+                            ),
+                            SizedBox(height: 5),
+                            Text('Hi, John! How are you doing?'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: CustomBottomNavBar(
         selectedIndex: _selectedIndex,
