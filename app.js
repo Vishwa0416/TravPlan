f36e6a90a178416a7e84ec8b9356fc6c379071f1
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-12-26 18:18:17 +0530

    Nodemon activation

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
 
-const app = express;
+const app = express();
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const app = require('./app');
 
 const port = 3000;
+
+app.get('/',(req,res)=>{
+    res.send("Hello World!")
+});
+
+app.listen(port,()=>{
+    console.log(`Server Listening on Port http://localhost:${port}`);
+});
