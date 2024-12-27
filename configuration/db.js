10707116ddd0949fd5c89e5ee0d2d9a47b0d4503
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-12-27 14:34:37 +0530

    Connection message

--- configuration/db.js
+++ configuration/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection('mongodb://localhost:27017');
+const connection = mongoose.createConnection(`mongodb://127.0.0.1:27017/ToDoDB`).on('open',()=>{console.log("MongoDB Connected");}).on('error',()=>{
+    console.log("MongoDB Connection error");
+});
 
-model.export = connection;
+module.exports = connection;
