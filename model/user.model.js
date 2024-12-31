3f0d9e37d5a115c9fa06e93c350c1ab5939d63f9
Vishwa Wijesakare <agvawijesekara@std.appsc.sab.ac.lk>
2024-12-31 14:54:32 +0530

    User schema

--- model/user.model.js
+++ model/user.model.js
@@ -4,5 +4,19 @@ const db = require('../configuration/db');
 const { Schema } = mongoose;
 
 const userSchema = new Schema ({
-    
+    username :{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    email :{
+        type:String,
+        lowercase:true,
+        required:true,
+        unique:true,
+    },
+    password :{
+        type:String,
+        required:true,
+    }
 })
